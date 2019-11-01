1fc5251c6e1e4afdbf3832c9f1e7311663983d0a
Jamila Aliyeva <padma@email.com>
2019-11-01 09:55:35 -0700

    will implement .splice in js to remove all number 3's with a string, working on string repeating only once

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,20 @@
+// front-end
+var newstr = [];
+
+// back-end
+
 $(document).ready(function() {
   $("#formOne").submit(function(event){
     event.preventDefault();
     var input = parseInt($("input#numbers").val());
-    var newstr = ""
-    for (var index = 0; index < input.length; i++) {
-      if (parseInt(input[index]) === 1) {
-        newstr += "Beep!";
+    var inputToString = input.toString();
+
+    for (var i = 0; i <= inputToString.length; i++){
+      if(inputToString.includes(3)) {
+        newstr.splice(i,1,"I'm sorry Dave, I'm afraid I can't do that");
         console.log(newstr);
-
-      }
-    }
-
-});
+        $(".results").text(newstr);
+      };
+    };
+  });
 });
